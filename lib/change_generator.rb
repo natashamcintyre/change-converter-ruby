09f4bcf84c3e5d869b24426f74f43629860b792b
Natasha McIntyre <natashamc.cs@gmail.com>
2021-03-24 19:49:44 +0000

    converts change for whole pound inputs

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -1,7 +1,15 @@
 class ChangeGenerator
 
   def convert(n)
-    ["£1"]
+    coins = [50, 20, 10, 5, 1, 0.5]
+    change = []
+    coins.each do |coin|
+      while n >= coin do
+        coin < 1 ? change << "#{(coin * 100).to_i}p" : change << "£#{coin}"
+        n -= coin
+      end
+    end
+    change
   end
 
 end
--- spec/change_generator_spec.rb
+++ spec/change_generator_spec.rb
@@ -5,5 +5,37 @@ describe ChangeGenerator do
     it "returns ['£1'] for an input of 1" do
       expect(subject.convert(1)).to eq(["£1"])
     end
+
+    it "returns ['£1', '£1'] for an input of 2" do
+      expect(subject.convert(2)).to eq(["£1", "£1"])
+    end
+
+    it "returns ['£1', '£1', '£1'] for an input of 3" do
+      expect(subject.convert(3)).to eq(["£1", "£1", "£1"])
+    end
+
+    it "returns ['£5'] for an input of 5" do
+      expect(subject.convert(5)).to eq(["£5"])
+    end
+
+    it "returns ['£5', '£1'] for an input of 6" do
+      expect(subject.convert(6)).to eq(["£5", "£1"])
+    end
+
+    it "returns ['£20', '£20'] for an input of 40" do
+      expect(subject.convert(40)).to eq(["£20", "£20"])
+    end
+
+    it 'returns ["£50", "£20", "£10", "£5", "£1"] for an input of 86' do
+      expect(subject.convert(86)).to eq(["£50", "£20", "£10", "£5", "£1"])
+    end
+
+    it 'returns ["£1", "50p"] for an input of 1.50' do
+      expect(subject.convert(1.50)).to eq(["£1", "50p"])
+    end
+
+    it 'returns ["£1", "50p", "20p"] for an input of 1.70' do
+      expect(subject.convert(1.70)).to eq(["£1", "50p", "20p"])
+    end
   end
 end
