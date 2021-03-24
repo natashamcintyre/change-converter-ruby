07b6da1e29d16309869cd86179d8930a65d279a2
Natasha McIntyre <natashamc.cs@gmail.com>
2021-03-24 20:05:40 +0000

    refactor coins to constant

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -1,16 +1,20 @@
 class ChangeGenerator
 
+  COINS = [5000, 2000, 1000, 500, 100, 50, 20, 10, 5]
+
+  def initialize
+    @change = []
+  end
+
   def convert(n)
     value = 100 * n
-    coins = [5000, 2000, 1000, 500, 100, 50, 20, 10, 5]
-    change = []
-    coins.each do |coin|
+    COINS.each do |coin|
       while value >= coin do
-        coin < 100 ? change << "#{coin}p" : change << "£#{coin/100}"
+        coin < 100 ? @change << "#{coin}p" : @change << "£#{coin/100}"
         value -= coin
       end
     end
-    change
+    @change
   end
 
 end
